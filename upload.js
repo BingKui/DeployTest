bc2e859b55aa1d08b6f979f9df62456804d5a04c
康兵奎 <178974662@qq.com>
2022-08-24 17:34:11 +0800

    test update

--- upload.js
+++ upload.js
@@ -1,10 +1,20 @@
-// const Minio = require('minio');
-// const {  } = process.env;
-// var minioClient = new Minio.Client({
-//     endPoint: 'play.min.io',
-//     port: 9000,
-//     useSSL: true,
-//     accessKey: 'Q3AM3UQ867SPQQA43P2F',
-//     secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
-// });
-console.log('系统变量 ->', process.env);
+const Minio = require('minio');
+const { MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY } = process.env;
+var minioClient = new Minio.Client({
+    endPoint: MINIO_ENDPOINT,
+    port: 443,
+    useSSL: true,
+    accessKey: MINIO_ACCESS_KEY,
+    secretKey: MINIO_SECRET_KEY
+});
+const file = './public/version.json';
+var metaData = {
+    'Content-Type': 'application/octet-stream',
+    'X-Amz-Meta-Testing': 1234,
+    'example': 5678
+}
+// Using fPutObject API upload your file to the bucket europetrip.
+minioClient.fPutObject('app', 'version.json', file, metaData, function(err, etag) {
+  if (err) return console.log(err)
+  console.log('文件上传成功.')
+});
